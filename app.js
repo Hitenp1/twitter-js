a64d685f7715e03eb21cd674b741a2cbdbe14df6
Sara Sunshine <sarasunsh@gmail.com>
2016-09-19 17:20:06 -0400

    Set up basic app

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var express = require( 'express' );
 var app = express(); // creates an instance of an express application
 
+app.use(function middle1(req, res,next){
+	console.log('/is-anybody-in-there');
+	next();
+});
 
-app.listen(8080);
+app.use('/news', function middle2(req, res, next){
+	console.log('/modernism');
+	console.log(res.statusCode);
+	next();
+});
+
+app.get('/', function(req, res, next){
+	res.sendStatus(200);
+});
+
+app.get('/news', function(req, res, next){
+	res.send('Hello World test');
+});
+
+
+app.listen(3000, function(){
+	console.log('You have connected')
+});
