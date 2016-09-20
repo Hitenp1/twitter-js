30ba0638c34b692ba70f85d7f3dc547d5fbe10a7
Hitenp1 <hpduke2@gmail.com>
2016-09-20 09:47:23 -0400

    Finsihed 8

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,42 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
+  res.render( 'index', { tweets: tweets, showForm: true } );
 });
+
+
+router.post('/tweets',urlencodedParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400)
+  var name = req.body.name;
+  var text = req.body.text;
+  console.log(name, text);
+  tweetBank.add(name, text);
+  res.redirect('/');
+});
+
 
 router.get('/users/:username', function(req, res) {
   var user = req.params.username;
   var list = tweetBank.find( {name: user} );
   res.render( 'index', { tweets: list } );
 });
+
+
+
 
 // router.get('/stylesheets/style.css', function (req, res) {
 // 	res.sendFile(__dirname.slice(0,-6) + '/public/stylesheets/style.css');
